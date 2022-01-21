8c5766cbdee5305ffb5cdcac41891b050267f516
Nirmal <nirmalrak@gmail.com>
2022-01-20 23:41:12 -0500

    added files

--- test.py
+++ test.py
@@ -1,5 +1,55 @@
-n = int(input('Enter integer n (>=1): '))
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from typing import Iterable, Callable
 
-for i in range (1, n+1):
-	for j in range (1, i+1):
-		print('The value of r is', j)
+import numpy as np
+import sys
+
+# Computes the distances between an NxD matrix and an MxD matrix and returns an NxM matrix of distances.
+distanceFn = Callable[[np.ndarray, np.ndarray], np.ndarray]
+
+
+class KNN:
+    # Fill in your KNN implementation here.
+
+    @property
+    def nearest_k(self) -> np.ndarray:
+        """Returns a numpy array of size [K x D] where K is the number of nearest
+        neighbors we want to keep and D is the dimensionality. The queries must
+        be sorted from smallest distance to largest distance (to any reference point).
+        """
+        pass
+
+
+def get_nearest_neighbors(
+        references: np.ndarray,  # Shape=[N, D]
+        distance_function_name: str,
+        k: int,
+        queries: Iterable[np.ndarray]
+) -> np.ndarray:
+    assert distance_function_name in ('L2', 'cityblock')
+
+    # Create the distance function which takes the query matrix (B, D) and the reference
+    # matrix (N, D) and produces a matrix of distances between all pairs of size (B, N).
+    #
+    # my_distance_fn = ...
+    #
+    # Your solution should look something like this:
+    # knn = KNN(references, distanceFn=my_distance_fn, K)
+    # for query in queries:
+    #    # The shape of query = [B, D].
+    #    knn.observe(query)
+    # return knn.nearest_k
+
+
+import json, os, sys
+
+if __name__ == "__main__":
+    config = json.load(sys.stdin)
+    reference_points = np.array(config["reference_points"])
+    distance_function_name = config["distance_function"]
+    k = config["k"]
+    queries = [np.array(query) for query in config["queries"]]
+    actual_nearest_k = get_nearest_neighbors(reference_points, distance_function_name, k, queries)
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    fptr.write(str(actual_nearest_k.astype(float).tolist()))
+    fptr.close()
