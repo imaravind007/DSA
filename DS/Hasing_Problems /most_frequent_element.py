71a2768c7a4d4f2ed62a4ac34d2bedbff39510f4
imaravind007 <73240959+imaravind007@users.noreply.github.com>
2022-02-07 13:29:00 -0600

    1

---
+++ DS/Hasing_Problems /most_frequent_element.py
@@ -0,0 +1,27 @@
+def first_maximum_repeating_element(arr):
+
+  dic = {}
+  for idx, value in enumerate(arr):
+    if value not in dic:
+      dic[value]=1
+    else:
+      dic[value] += 1
+  print(dic)
+  # return dic[1]
+
+  # Maximum frequency:
+  max_count = 0
+  for idx, value in enumerate(arr):
+    if (max_count < dic[value]):
+      max_count = dic[value]
+  
+
+  return max_count
+
+
+    
+ 
+
+arr = [4,4,4,4,1,1,1,1,0,0,0,0,0,0,0]
+output = first_maximum_repeating_element(arr)
+print(output)
